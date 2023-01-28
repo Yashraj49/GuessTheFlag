0334b2f9d790697837a7c0d6f8addf9d76d9e99b
Yashraj Jadhav <yashrajjadhav49@gmail.com>
2023-01-28 12:53:28 +0530

    Added Animations , changed  a bit of UI .

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -27,15 +27,25 @@ struct ContentView: View {
     @State private var countries = [ "India", "Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
     
+    @State private var spinAnimationAmounts = [0.0 , 0.0 , 0.0 ]
+    @State private var animatingIncreaseScore = false
+    
+    @State private var shakeAnimationAmounts = [0, 0, 0]
+    @State private var animatingDecreaseScore = false
+    
+    @State private var animateopacity = false
+    @State private var allowSubmitAnswer = true
+    
+    
     var body: some View {
         ZStack {
             RadialGradient(stops: [
                 .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
                 .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3)
             ], center: .top, startRadius: 200, endRadius: 700)
-            .ignoresSafeArea()
+            .ignoresSafeArea(.all)
             
-            VStack {
+            VStack() {
                 Spacer()
                 
                 Text("Guess the Flag")
@@ -50,15 +60,23 @@ struct ContentView: View {
                         
                         Text(countries[correctAnswer])
                             .font(.largeTitle.weight(.semibold))
+                            .fontWeight(.semibold).font(.callout)
+                        
                     }
+                    
                     
                     ForEach(0 ..< 3) { number in
                         Button(action: {
                             self.flagTapped(number)
-                        }) {
+                        })
+                        {
                             
                             // project 3 - challenge 3
                             FlagImage(name: self.countries[number])
+                            // project 6 - challenge 1
+                            
+                                .rotation3DEffect(.degrees(self.spinAnimationAmounts[number]), axis: (x: 0, y: 1, z: 0))
+                            
                         }
                     }
                 }
@@ -71,39 +89,100 @@ struct ContentView: View {
                 Spacer()
                 
                 Text("Score:\(userScore)")
-                    .foregroundColor(.white)
+                    .foregroundColor(animatingIncreaseScore ? .green : (animatingDecreaseScore ? .red : .white))
+                // .foregroundColor(.white)
                     .font(.title.bold())
+                
+                // project 6 - challenge 1
+                Text("+1")
+                    .font(.headline)
+                    .foregroundColor(animatingIncreaseScore ? .green : .clear)
+                    .opacity(animatingIncreaseScore ? 0 : 1)
+                    .offset(x: 0, y: animatingIncreaseScore ? -50 : -20)
+                
+                Text("-1")
+                                            .foregroundColor(animatingDecreaseScore ? .red : .clear)
+                                            .font(.headline)
+                                            .opacity(animatingDecreaseScore ? 0 : 1)
+                                            .offset(x: 0, y: animatingDecreaseScore ? 50 : 20)
                 
                 Spacer()
             }
             .padding()
+            
         }
         .alert(scoreTitle, isPresented: $showingScore) {
             Button("Continue", action: askQuestion)
         }
         
     message: {
-        Text("Your score is \(userScore)")
+        Text("Your score is \(userScore)").font(.headline).foregroundColor(animatingDecreaseScore ?.red : .clear)
         
     }
     }
+
     
     func flagTapped(_ number: Int) {
+        
+        // project 6 - challenge 2
+   
+        
+        let nextQuestionDelay = 1.5
+        
+        let flagAnimationDuration = 0.5
+        
+        let scoreUpdateDuration = 1.5
+        
+        withAnimation(Animation.easeOut(duration: flagAnimationDuration)){
+            self.animateopacity = true
+        }
+        
         if number == correctAnswer {
             scoreTitle = "Correct"
             userScore += 1
-        } else {
+            
+            withAnimation(Animation.easeInOut(duration: flagAnimationDuration)) {
+            /* this was imp here */                self.spinAnimationAmounts[number] += 360
+                        }
+                        withAnimation(Animation.linear(duration: scoreUpdateDuration)) {
+                            self.animatingIncreaseScore = true
+                        }
+                    }
+            
+        
+    else {
             scoreTitle = "Wrong! That’s the flag of \(countries[number])"
-            userScore += 1
+            userScore -= 1
+        
+        withAnimation(Animation.easeOut(duration: flagAnimationDuration)) {self.shakeAnimationAmounts[number] = 2}
+        
         }
         
+        withAnimation(Animation.linear(duration: scoreUpdateDuration)) {
+                        self.animatingDecreaseScore = true
+                    }
+        
         showingScore = true
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + nextQuestionDelay) {
+                    self.askQuestion()
+                }
+        
     }
     
     func askQuestion() {
+       
+        
+        self.spinAnimationAmounts = [0.0, 0.0, 0.0]
+        self.animatingIncreaseScore = false
+        
+        self.animateopacity = false
+        
+        self.shakeAnimationAmounts = [0, 0, 0]
+                self.animatingDecreaseScore = false
+        
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
-        
         
     }
 }
